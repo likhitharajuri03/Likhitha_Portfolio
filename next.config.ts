0f83fe4d7f342f86f6510450318cd6706549e25c
Likhi <likhitha.rajuri.03@gmail.com>
2026-07-08 23:40:40 +0530

    changed config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProd ? "/Likhitha_Portfolio" : "",
+  assetPrefix: isProd ? "/Likhitha_Portfolio/" : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
